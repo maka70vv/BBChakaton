9ccd674498e4ab78c796197489826abc9b813292
Makarov <25.makarovv@gmail.com>
2023-11-04 12:39:13 +0600

    requirements added

--- hakatonDjango/settings.py
+++ hakatonDjango/settings.py
@@ -94,14 +94,21 @@ WSGI_APPLICATION = 'hakatonDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'zakupki',
-        'USER': 'root',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'zakupki',
+#         'USER': 'root',
+#         'PASSWORD': '123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
