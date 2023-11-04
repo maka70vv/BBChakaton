75b620410d314cc069f09d4e53de4b1449fa4d0d
Albrut <115610709+Albrut@users.noreply.github.com>
2023-11-04 13:02:51 +0600

    New

--- parser/apps.py
+++ parser/apps.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponse
 from django.apps import AppConfig
-
-
-class ParserConfig(AppConfig):
+import os
+class Myappconfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'parser'
+    name = 'mysite'
+    a = 0
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN'):
+            print('Hello')
+        else: pass
+
